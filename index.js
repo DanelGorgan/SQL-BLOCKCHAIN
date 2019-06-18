dff71c0d8afb3f6b96840e8f83b43c9aa1db6656
DanelGorgan <danielgorganmihai@gmail.com>
2019-06-18 23:13:45 +0300

    changed connection

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const sql = require('mssql')
+const sql = require('mssql');
+
+const config = {
+    user: '...',
+    password: '...',
+    server: 'localhost',
+    database: 'master',
+
+};
 
 async () => {
     try {
         // await sql.connect('mssql://username:password@localhost/database');
-        await sql.connect('mssql://DGORGAN-L:@localhost/master');
-        const result = await sql.query`select * from mytable where id = ${value}`;
+        await sql.connect(config);
+        const result = await sql.query`select * from mytable`;
         console.dir(result)
     } catch (err) {
-        // ... error checks
+        console.error(err)
     }
 };
